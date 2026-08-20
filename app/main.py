731bc13f2d0adb765708a2578e54291f339a52f8
Emirhan <emirhan.uzen@gmail.com>
2026-08-20 12:18:28 +0300

    CORS middleware eklendi (frontend entegrasyonu için)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import import_gtfs,agency,route,stop,stop_time,trip,export
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(title="GTFS-VALİDATOR-PROJECT")
 
@@ -10,3 +11,11 @@ app.include_router(stop.router,tags=["stop"])
 app.include_router(stop_time.router,tags=["stop_time"])
 app.include_router(trip.router,tags=["trip"])
 app.include_router(export.router,tags=["export"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
